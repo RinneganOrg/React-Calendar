063285719a2d8fc20d7b32e42d802e5bf1931878
MihaelaCretu11 <cretu.mihaelaa11@gamil.com>
2022-10-19 17:34:49 +0300

    feat: implemented view/edit event on click, update event on drag and changing month with arrow buttons and drop down

--- src/CoursesCalendar.jsx
+++ src/CoursesCalendar.jsx
@@ -1,6 +1,7 @@
-import './coursesCalendar.css';
-import React, { useState } from 'react';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import "./coursesCalendar.css";
+import React, { useState, useEffect } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import moment from "moment";
 
 const months = [
   { key: 0, daysInMonth: 31, value: 1, text: "January" },
@@ -14,114 +15,280 @@ const months = [
   { key: 8, daysInMonth: 30, value: 9, text: "September" },
   { key: 9, daysInMonth: 31, value: 10, text: "October" },
   { key: 10, daysInMonth: 30, value: 11, text: "November" },
-  { key: 11, daysInMonth: 31, value: 12, text: "December" }
+  { key: 11, daysInMonth: 31, value: 12, text: "December" },
 ];
 const daysOfTheWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-const allEvents = [
-  {id: 'project', content: 'Projects', className: 'task task--warning'},
-  {id: 'design-sprint', content: 'Design Sprint', className: 'task task--danger'},
-  {id: 'product-checkup', content: 'Product Checkup 1', className: 'task task--primary'},
-]
-
 /**
  * Will make an array containing last days of previous month, all days of current month and first days of next month
- * depending on when does the first/last day of current month occur during week 
+ * depending on when does the first/last day of current month occur during week
  * (if 1 is Monday => no previous month days, 31 is Sunday => no next month days)
- * @param {Number} selectedMonthIndex 
- * @returns 
+ * @param {Number} selectedMonthIndex
+ * @returns
  */
-const fillCalendarDays = (selectedMonthIndex) => {
-  let selectedMonthItem = months[selectedMonthIndex];
 
-  const selectedMonthDays1 = Array(selectedMonthItem.daysInMonth)
+const selectedMonthDaysWithEvents = (
+  selectedMonthItem,
+  selectedYear,
+  selectedMonthIndex,
+  events
+) => {
+  const selectedMonthDays = Array(selectedMonthItem.daysInMonth)
     .fill(0)
-    .map((day, i) => ({
-      dayNumber: i + 1,
+    .map((_, index) => ({
+      year: selectedYear,
+      dayNumber: index + 1,
       class: "day",
       month: selectedMonthIndex + 1,
-      events: []
-    }))
+      events: [],
+    }));
+  return selectedMonthDays.map((selectedMonthDay) => {
+    selectedMonthDay.events = events.filter(
+      (event) =>
+        moment(event.startDate).date() === selectedMonthDay.dayNumber &&
+        moment(event.startDate).month() + 1 === selectedMonthDay.month &&
+        moment(event.startDate).year() === selectedMonthDay.year
+    );
+    return selectedMonthDay;
+  });
+};
 
-    const [day1, day2, ...rest] = selectedMonthDays1;
-    const selectedMonthDays2 = {...day1, events: allEvents.slice(0, 1 + Math.floor(Math.random() * 3))};
-    const selectedMonthDays3 = {...day2, events: allEvents.slice(0, 1 + Math.floor(Math.random() * 3))};
-    const selectedMonthDays = [selectedMonthDays2, selectedMonthDays3, ...rest];
-
-  let firstDayOfTheMonthDate = `${selectedMonthIndex + 1} 1 2022`;
+const previousMonthDisabledDays = (selectedMonthIndex, selectedYear) => {
+  let firstDayOfTheMonthDate = `${selectedMonthIndex + 1} 1 ${selectedYear}`;
   let firstDayOfTheMonthWeekIndex = new Date(firstDayOfTheMonthDate).getDay();
-
-  const previousMonthDisabledDays = Array(
+  return Array(
     firstDayOfTheMonthWeekIndex === 0 ? 6 : firstDayOfTheMonthWeekIndex - 1 // Sunday is 0 Saturday is 6
   )
     .fill(0)
-    .map((previousMonthDisabledDay, i) => {
-      const previousMonth = selectedMonthIndex > 0 ? months[selectedMonthIndex - 1] : months[11]; // if current month is Jan previous month is Dec
+    .map((_, index) => {
+      const previousMonth =
+        selectedMonthIndex > 0 ? months[selectedMonthIndex - 1] : months[11]; // if current month is Jan previous month is Dec
 
-      return {
-        dayNumber: previousMonth.daysInMonth - i,
-        class: "day day--disabled",
-        month: selectedMonthIndex,
-        events: []
-      }
-    });
+      return selectedMonthIndex > 0
+        ? {
+            year: selectedYear,
+            dayNumber: previousMonth.daysInMonth - index,
+            class: "day day--disabled",
+            month: selectedMonthIndex,
+            events: [],
+          }
+        : {
+            year: selectedYear - 1,
+            dayNumber: previousMonth.daysInMonth - index,
+            class: "day day--disabled",
+            month: 12,
+            events: [],
+          };
+    })
+    .reverse();
+};
 
-  let lastDayOfTheMonthDate = `${selectedMonthIndex + 1} ${selectedMonthItem.daysInMonth} 2022`;
+const nextMonthDisabledDays = (
+  selectedMonthItem,
+  selectedMonthIndex,
+  selectedYear
+) => {
+  let lastDayOfTheMonthDate = `${selectedMonthIndex + 1} ${
+    selectedMonthItem.daysInMonth
+  } ${selectedYear}`;
   let lastDayOfTheMonthWeekIndex = new Date(lastDayOfTheMonthDate).getDay();
-
-  let nextMonthDisabledDays = Array(7 - lastDayOfTheMonthWeekIndex)
+  return Array(7 - lastDayOfTheMonthWeekIndex)
     .fill(0)
-    .map((nextMonthDisabledDay, i) => ({
-      dayNumber: i + 1,
-      class: "day day--disabled",
-      month: selectedMonthIndex + 2,
-      events: []
-    }));
+    .map((_, index) =>
+      selectedMonthIndex < 11
+        ? {
+            year: selectedYear,
+            dayNumber: index + 1,
+            class: "day day--disabled",
+            month: selectedMonthIndex + 2,
+            events: [],
+          }
+        : {
+            year: selectedYear + 1,
+            dayNumber: index + 1,
+            class: "day day--disabled",
+            month: 1,
+            events: [],
+          }
+    );
+};
+
+const fillCalendarDays = (selectedMonthIndex, events, selectedYear) => {
+  let selectedMonthItem = months[selectedMonthIndex];
 
   const result = [
-    ...previousMonthDisabledDays.reverse(),
-    ...selectedMonthDays,
-    ...nextMonthDisabledDays
+    ...previousMonthDisabledDays(selectedMonthIndex, selectedYear),
+    ...selectedMonthDaysWithEvents(
+      selectedMonthItem,
+      selectedYear,
+      selectedMonthIndex,
+      events
+    ),
+    ...nextMonthDisabledDays(
+      selectedMonthItem,
+      selectedMonthIndex,
+      selectedYear
+    ),
   ];
   return result;
 };
 
-const CoursesCalendar = ({ gymId, userId }) => {
+const makeInterval = (year, month, startDay, endDay) => {
+  return {
+    startDate: moment([year, month.key, startDay]),
+    endDate: moment([year, month.key, endDay]),
+  };
+};
+
+//remove dragged event from source
+const removeDraggedEvent = (sourceDay, source) => {
+  return [
+    ...sourceDay.events.slice(0, source.index),
+    ...sourceDay.events.slice(source.index + 1),
+  ];
+};
+
+// add dropped event to destination at desired position
+const addDroppedEvent = (destinationDay, eventToMove, destination) => {
+  return [
+    ...destinationDay.events.slice(0, destination.index),
+    eventToMove,
+    ...destinationDay.events.slice(destination.index),
+  ];
+};
+
+// change startDate and endDate for the dropped event
+const updateEventDates = (eventToMove, destinationDay, editEventData) => {
+  const { id, title } = eventToMove;
+  const diff =
+    new Date(eventToMove.endDate).getTime() -
+    new Date(eventToMove.startDate).getTime();
+  const startDate = moment([
+    destinationDay.year,
+    destinationDay.month - 1,
+    destinationDay.dayNumber,
+  ]).format("YYYY-MM-DD");
+  const endDate = moment(new Date(startDate).getTime() + diff).format(
+    "YYYY-MM-DD"
+  );
+  return editEventData({ id, title, startDate, endDate });
+};
+
+const CoursesCalendar = ({
+  events = [],
+  modalPopUp: ModalPopUp,
+  handleOpenModal,
+  fetchEventsByInterval,
+  getCurrentEventById,
+  makeDefaultEvent,
+  editEventData,
+}) => {
+  useEffect(() => {
+    fetchEventsByInterval(
+      makeInterval(selectedYear, selectedMonth, 1, selectedMonth.daysInMonth)
+    );
+  }, []);
+
   const [selectedMonth, setCurrentMonth] = useState(
     months[new Date().getMonth()]
   );
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
   const [daysOfTheMonth, setDaysOfTheMonth] = useState(
-    fillCalendarDays(new Date().getMonth())
+    fillCalendarDays(new Date().getMonth(), events, selectedYear)
   );
 
-  const onChangeMonth = (monthSelection) => {
+  const onChangeMonth = (monthSelection, year) => {
     setCurrentMonth(monthSelection);
-    setDaysOfTheMonth(fillCalendarDays(monthSelection.key));
+    fetchEventsByInterval(
+      makeInterval(year, monthSelection, 1, monthSelection.daysInMonth)
+    );
+  };
+  const setNextYear = () => {
+    const year = selectedYear + 1;
+    setSelectedYear(year);
+    onChangeMonth(months[0], year);
+  };
+  const setPreviousYear = () => {
+    const year = selectedYear - 1;
+    setSelectedYear(year);
+    onChangeMonth(months[11], year);
+  };
+  const handleCreate = (selectedYear, selectedMonth, dayOfTheMonth) => {
+    makeDefaultEvent(
+      makeInterval(
+        selectedYear,
+        selectedMonth,
+        dayOfTheMonth.dayNumber,
+        dayOfTheMonth.dayNumber
+      )
+    );
+    handleOpenModal();
+  };
+  const handleEdit = (eventId) => {
+    getCurrentEventById(eventId);
+    handleOpenModal();
+  };
+  const handleChangePreviousMonth = (selectedMonth, selectedYear) => {
+    selectedMonth.key > 0
+      ? onChangeMonth(months[selectedMonth.key - 1], selectedYear)
+      : setPreviousYear();
+  };
+  const handleChangeNextMonth = (selectedMonth, selectedYear) => {
+    selectedMonth.key < 11
+      ? onChangeMonth(months[selectedMonth.key + 1], selectedYear)
+      : setNextYear();
   };
 
-  const monthRadioSelector = (
-    <>
-      <div className="calendar-header">
-        <h1>
-          {selectedMonth.text}
-          <button>▾</button>
-        </h1>
-        <p>2022</p>
-      </div>
-      <div>
-        {months.map((month, i) => (
-          <span key={`key-${month.key}`}>
-            <input
-              type="radio"
-              value={month.key}
-              name="gender"
-              checked={month.key === selectedMonth.key}
-              onChange={() => onChangeMonth(month)}
-            />
-            {`${month.text} `}
-          </span>
+  useEffect(() => {
+    setDaysOfTheMonth(
+      fillCalendarDays(selectedMonth.key, events, selectedYear)
+    );
+  }, [events, selectedMonth.key, selectedYear]);
+
+  const monthSelector = (
+    <div className="calendar-header">
+      <button
+        className="calendar-header-arrow-btn"
+        onClick={(e) => handleChangePreviousMonth(selectedMonth, selectedYear)}
+      >
+        <img src="https://i.imgur.com/2gqThFI.png" alt="left-arrow" />
+      </button>
+      <select
+        className="calendar-header-month-selection"
+        name="month-selection"
+        value={selectedMonth.key}
+        onChange={(e) => {
+          onChangeMonth(months[e.target.value], selectedYear);
+        }}
+      >
+        {months.map((month) => (
+          <option
+            className="calendar-header-month-options"
+            value={month.key}
+            key={`key-${month.key}`}
+          >
+            {month.text} {selectedYear}
+          </option>
         ))}
-      </div>
-    </>)
+      </select>
+
+      <button
+        className="calendar-header-arrow-btn"
+        onClick={() => handleChangeNextMonth(selectedMonth, selectedYear)}
+      >
+        <img src="https://i.imgur.com/uambqYY.png" alt="right-arrow" />
+      </button>
+      <button
+        onClick={() => {
+          setSelectedYear(new Date().getFullYear());
+          const year = new Date().getFullYear();
+          onChangeMonth(months[new Date().getMonth()], year);
+        }}
+        className="calendar-header-today-btn"
+      >
+        Today
+      </button>
+    </div>
+  );
   /**
    * Week days names ex: Monday etc
    */
@@ -129,7 +296,8 @@ const CoursesCalendar = ({ gymId, userId }) => {
     <span key={`key-${i}`} className="day-name">
       {dayOfTheWeek}
     </span>
-  ))
+  ));
+
   /**
    * Move the event to different destination at selected position or keep it in place
    * (will not reorder if source is the same as destination)
@@ -147,7 +315,6 @@ const CoursesCalendar = ({ gymId, userId }) => {
    */
   const onDragEnd = (dragResult) => {
     const { source, destination } = dragResult;
-
     // if destination is not droppable or source is the same as destination, it will be kept in place
     if (!destination || destination?.droppableId === source?.droppableId) {
       return;
@@ -155,78 +322,84 @@ const CoursesCalendar = ({ gymId, userId }) => {
 
     // make copy to make it safe to mutate, the source won't be changed
     const allDaysCurrentMonth = daysOfTheMonth.slice();
-  
-    //remove dragged event from source
+
     const sourceDay = allDaysCurrentMonth[source.droppableId];
-    const eventToMove = sourceDay.events[source.index];
-    const updatedSourceEvents = [
-      ...sourceDay.events.slice(0, source.index),
-      ...sourceDay.events.slice(source.index+1)
-    ];
-
-    // add dropped event to destination at desired position
     const destinationDay = allDaysCurrentMonth[destination.droppableId];
-    const updatedDestinationEvents = [
-      ...destinationDay.events.slice(0, destination.index),
-      eventToMove,
-      ...destinationDay.events.slice(destination.index)
-    ];
+    const eventToMove = sourceDay.events[source.index];
 
-    // replacing item in array is safe to mutate, 
+    // replacing item in array is safe to mutate,
     // it won't change the original source
     allDaysCurrentMonth[source.droppableId] = {
       ...sourceDay,
-      events: updatedSourceEvents
+      events: removeDraggedEvent(sourceDay, source),
     };
 
     allDaysCurrentMonth[destination.droppableId] = {
       ...destinationDay,
-      events: updatedDestinationEvents
+      events: addDroppedEvent(destinationDay, eventToMove, destination),
     };
 
+    updateEventDates(eventToMove, destinationDay, editEventData);
+
     setDaysOfTheMonth(allDaysCurrentMonth);
-  }
+  };
 
   const dayCells = daysOfTheMonth.map((dayOfTheMonth, dayIndex) => {
     return (
-    <Droppable droppableId={`${dayIndex}`} key={`key-${dayIndex}`}>
-      {(provided) => (
-        <section
-          className={dayOfTheMonth.class}
-          {...provided.droppableProps}
-          ref={provided.innerRef}
-        >
-          <div>
-            {dayOfTheMonth.dayNumber}
-            {provided.placeholder}
-            {dayOfTheMonth.events.map((event, eventIndex) => (
-              <Draggable key={`key-${eventIndex}`} draggableId={`day-${dayOfTheMonth.dayNumber}-${event.id}`} index={eventIndex}>
-                {(provided, snapshot) => (
-                  <section
-                    className={event.className}
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    {event.content}
-                  </section>
-                )}
-              </Draggable>
-            ))}
-          </div>
-        </section>
-      )}
-    </Droppable>
-  )})
+      <Droppable droppableId={`${dayIndex}`} key={`key-${dayIndex}`}>
+        {(provided) => (
+          <section
+            className={dayOfTheMonth.class}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            <div>
+              <div
+                onClick={() =>
+                  handleCreate(selectedYear, selectedMonth, dayOfTheMonth)
+                }
+              >
+                {dayOfTheMonth.dayNumber}
+                {provided.placeholder}
+              </div>
+              {dayOfTheMonth.events.map((event, eventIndex) => (
+                <Draggable
+                  key={`key-${eventIndex}`}
+                  draggableId={`day-${dayOfTheMonth.dayNumber}-${event.id}`}
+                  index={eventIndex}
+                >
+                  {(provided, snapshot) => (
+                    <section
+                      onClick={() => {
+                        handleEdit(event.id);
+                      }}
+                      // className={event.className}
+                      className="task task--primary"
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {event.title}
+                    </section>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          </section>
+        )}
+      </Droppable>
+    );
+  });
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="calendar-container">
-        {monthRadioSelector}
+        {monthSelector}
         <div className="calendar">
           {daysOfTheWeekIndicators}
           {dayCells}
         </div>
+        <ModalPopUp></ModalPopUp>
       </div>
     </DragDropContext>
   );
