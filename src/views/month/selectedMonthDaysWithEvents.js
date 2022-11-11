63dd9045d17100454da99e76203776518b707156
MihaelaCretu11 <cretu.mihaelaa11@gamil.com>
2022-11-11 12:29:58 +0200

    chore: clean up

--- src/views/month/selectedMonthDaysWithEvents.js
+++ src/views/month/selectedMonthDaysWithEvents.js
@@ -1,27 +1,27 @@
 import { makeEventsForDay } from "./makeEventsForDay";
 
 export const selectedMonthDaysWithEvents = (
-    selectedMonthItem,
-    selectedYear,
-    selectedMonthIndex,
-    events
-  ) => {
-    const selectedMonthDays = Array(selectedMonthItem.daysInMonth)
-      .fill(0)
-      .map((_, index) => {
-        const year = selectedYear;
-        const month = selectedMonthIndex + 1;
-        const dayNumber = index + 1;
-  
-        const result = {
-          year,
-          month,
-          dayNumber,
-          class: "day",
-          events: makeEventsForDay(year, month, dayNumber, events),
-        };
-  
-        return result;
-      });
-    return selectedMonthDays;
-  };
+  selectedMonthItem,
+  selectedYear,
+  selectedMonthIndex,
+  events
+) => {
+  const selectedMonthDays = Array(selectedMonthItem.daysInMonth)
+    .fill(0)
+    .map((_, index) => {
+      const year = selectedYear;
+      const month = selectedMonthIndex + 1;
+      const dayNumber = index + 1;
+
+      const result = {
+        year,
+        month,
+        dayNumber,
+        class: "day",
+        events: makeEventsForDay(year, month, dayNumber, events),
+      };
+
+      return result;
+    });
+  return selectedMonthDays;
+};
